17322f11b05d7e73f85549788cca15354b54afb3
Vidya Trimbake <vidyatrimbake3@gmail.com>
2025-10-13 22:28:04 +0530

    Add files via upload

---
+++ CompressStr.java
@@ -0,0 +1,21 @@
+package Java;
+
+public class CompressStr {
+    public static void main(String[] args) {
+        String str = "aaabjjjjj";
+        int cnt = 1;
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0;i<str.length();i++){
+            while(i<str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                cnt++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(cnt>1){
+                sb.append(cnt);
+            }
+            cnt = 1;
+        }
+        System.out.println(sb);
+    }
+}
